3a27a88846a2c4341a57ac122aec7d5e1c459efb
Nicole Babb <tom.erickson@deepspacedynamics.us>
2025-12-13 15:54:17 +0700

    Hide share button on Heliocentric tab

--- components/heliocentric/nasa-eyes-viewer.tsx
+++ components/heliocentric/nasa-eyes-viewer.tsx
@@ -3,7 +3,7 @@
 export default function NasaEyesViewer() {
   return (
     <iframe
-      src="https://eyes.nasa.gov/apps/solar-system/#/home?embed=true&logo=false&menu=false&featured=false"
+      src="https://eyes.nasa.gov/apps/solar-system/#/home?embed=true&logo=false&menu=false&featured=false&share=false"
       className="w-full h-full border-0"
       title="NASA Eyes on the Solar System"
       allow="fullscreen"
