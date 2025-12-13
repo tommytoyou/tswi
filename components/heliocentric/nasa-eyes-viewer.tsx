a35619021664cf2c1ffc14dc863955c134e676cb
Nicole Babb <tom.erickson@deepspacedynamics.us>
2025-12-13 16:07:44 +0700

    Cover NASA Eyes share button with overlay

--- components/heliocentric/nasa-eyes-viewer.tsx
+++ components/heliocentric/nasa-eyes-viewer.tsx
@@ -2,11 +2,23 @@
 
 export default function NasaEyesViewer() {
   return (
-    <iframe
-      src="https://eyes.nasa.gov/apps/solar-system/#/home?embed=true&logo=false&menu=false&featured=false&share=false"
-      className="w-full h-full border-0"
-      title="NASA Eyes on the Solar System"
-      allow="fullscreen"
-    />
+    <div className="relative w-full h-full">
+      <iframe
+        src="https://eyes.nasa.gov/apps/solar-system/#/home?embed=true&logo=false&menu=false&featured=false&share=false"
+        className="w-full h-full border-0"
+        title="NASA Eyes on the Solar System"
+        allow="fullscreen"
+      />
+      {/* Overlay to hide the share button in top right corner */}
+      <div
+        className="absolute top-0 right-0 bg-black"
+        style={{
+          width: '100px',
+          height: '50px',
+          pointerEvents: 'auto'
+        }}
+        aria-hidden="true"
+      />
+    </div>
   );
 }
